587c21a403b0eae0a7489cbfebab15be9c295c80
joe adcock <adcock.joe@gmail.com>
2025-02-04 21:01:16 -0500

    automatically set sent_at for now

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :user
+
+  after_create :set_sent_at
+
+  private
+
+  # Internal: assign sent_at for now, eventually this could be set by Twilio API
+  def set_sent_at
+    update sent_at: created_at
+  end
 end
